3708a4ff35a9222e8435054b621b63dcb21242f6
maksonix777 <128647093+maksonix777@users.noreply.github.com>
2023-07-01 22:43:31 +0200

    Add files via upload

---
+++ Practical.java
@@ -0,0 +1,23 @@
+package de.telran.HomeWork;
+
+import java.util.Scanner;
+
+public class Practical {
+    public static void main(String[] args) {
+        float num1;
+        float num2;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number one:");
+        num1 = scanner.nextFloat();
+        System.out.println("Enter a number one:");
+        num2 = scanner.nextFloat();
+        System.out.println(addition(num1, num2));
+
+    }
+    static float addition (float num1, float num2) {
+        float sum  =  num1 +  num2;
+        return  sum;
+    }
+
+}
