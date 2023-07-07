f2ee005f35584ebaf4c8d6765e0989b3488624c8
maksonix777 <128647093+maksonix777@users.noreply.github.com>
2023-07-07 09:06:32 +0200

    Update WorkingDay.java

--- WorkingDay.java
+++ WorkingDay.java
@@ -2,21 +2,13 @@ package de.telran.HomeWork;
 
 import java.util.Random;
 
-/*Домашнее задание №2
-Есть устройство, на табло которого показывается количество секунд,
- оставшихся до конца рабочего дня. Когда рабочий день начинается ровно в 9 часов утра
-  — табло отображает «28800» (т.е. остаётся 8 часов), когда 14:30 — на табло «9000»
-(т.е. остаётся два с половиной часа),
-а когда наступает 17 часов — на табло отображается «0» (т.е. рабочий день закончился).
-Некоторый сотрудники не умеют оценивать остаток рабочего дня в секундах.
-Требуется написать программу, которая вместо секунд будет выводить на табло
-понятные фразы с информацией о том, сколько полных часов осталось до конца рабочего дня.
-Например: «осталось 7 часов», «осталось 4 часа», «остался 1 час», «осталось менее часа».
-Объяснение: в переменную n должно записываться случайное (на время тестирования программы)
-целое число из диапазона от 0 до 28800, далее оно должно выводиться на экран
-(для тех, кто понимает в секундах) и на следующей строке (для тех кто не понимает)
-должна выводиться фраза о количестве полных часов, содержащихся в n секундах.
-*/
+//*Domashnee zadanie №2
+//Est' ustrojstvo, na tablo kotorogo pokazyvaetsja kolichestvo sekund,
+// ostavshihsja do konca rabochego dnja. Kogda rabochij den' nachinaetsja rovno v 9 chasov utra
+ // — tablo otobrazhaet «28800» (t.e. ostajotsja 8 chasov), kogda 14:30 — na tablo «9000»
+//(t.e. ostajotsja dva s polovinoj chasa),
+
+
 public class WorkingDay {
     public static void main(String[] args) {
         Random random = new Random();
@@ -45,3 +37,45 @@ public class WorkingDay {
     }
 }
 
+//Sozdat' programmu, vyvodjashhuju na jekran blizhajshee k 10 iz dvuh chisel, zapisannyh v peremennye m i n.
+//Chisla mogut byt', kak celochislennye, tak i drobnye.
+//Naprimer :
+//vvod : m=10.5, n=10.45
+//vyvod: Chislo 10.45 blizhe k 10.
+
+public class Task5 {
+    public static void main(String[] args) {
+        double m;
+        double n;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        m = scanner.nextDouble();
+
+        System.out.println("Enter first number:");
+        n = scanner.nextDouble();
+
+        double diffM = m - 10;
+        double diffN = n - 10;
+
+        if (diffM < 0)
+            diffM = -diffM;
+
+        if (diffN < 0)
+            diffN = -diffN;
+
+        if (diffM < diffN) {
+            System.out.println("Число " + m + " ближе к 10.");
+        } else if (diffN < diffM) {
+            System.out.println("Число " + n + " ближе к 10.");
+        } else {
+            System.out.println("Оба числа равноудалены от 10.");
+        }
+
+        scanner.close();
+
+        //  if (m > 10 && n > 10  && m < n) {
+
+
+    }
+}
